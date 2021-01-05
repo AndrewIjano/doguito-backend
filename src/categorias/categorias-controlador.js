56b6027f538919e7bc5842aa2ea63b524a94c206
Andrew Ijano <andrew.lopes@uol.com.br>
2021-01-05 12:09:06 -0300

    Fix wrong additions

--- src/categorias/categorias-controlador.js
+++ src/categorias/categorias-controlador.js
@@ -3,14 +3,6 @@ const Categoria = require('./categorias-modelo')
 module.exports = {
   async lista(req, res) {
     try {
-      const Categoria2 = require('./categorias-modelo')
-       
-      console.log(`Categoria:`) 
-      console.log(Categoria)
-      console.log(Categoria2)
-      console.log(`-----`)
-      console.log()
-
       const categorias = await Categoria.listarTodos()
       res.status(200).json(categorias)
     } catch (erro) {
@@ -21,9 +13,9 @@ module.exports = {
   async buscaCategoria(req, res) {
     try {
       const { categoriaNome } = req.params
-      const c = await Categoria.buscaPorNome(categoriaNome)
-      const subcategorias = await c.listaSubcategorias()
-      res.status(200).json({ ...c, subcategorias })
+      const categoria = await Categoria.buscaPorNome(categoriaNome)
+      const subcategorias = await categoria.listaSubcategorias()
+      res.status(200).json({ ...categoria, subcategorias })
     } catch (erro) {
       return res.status(500).json({ erro: erro.message })
     }

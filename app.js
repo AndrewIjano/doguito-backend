afc7df19453744509e4a611dd5f77a5afb15c1d7
Andrew Ijano <andrew.lopes@uol.com.br>
2021-01-06 11:20:12 -0300

    Add allowed headers

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
-app.use(cors())
+app.use(cors({ allowedHeaders: ['Content-Type', 'Authorization'] }))
 app.use(bodyParser.json())
 
 module.exports = app
